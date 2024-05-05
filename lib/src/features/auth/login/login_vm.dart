7fb1bf8a653e7973795353b52d5e8a3eb062bd4e
Nilo Dornellas <iannilo01@gmail.com>
2024-05-05 11:47:56 -0300

    Implementando o fluxo para mudar o state de acordo com o usuário logado

--- lib/src/features/auth/login/login_vm.dart
+++ lib/src/features/auth/login/login_vm.dart
@@ -3,6 +3,7 @@ import 'package:dw_barbershop/src/core/exceptions/service_exception.dart';
 import 'package:dw_barbershop/src/core/fp/either.dart';
 import 'package:dw_barbershop/src/core/providers/application_providers.dart';
 import 'package:dw_barbershop/src/features/auth/login/login_state.dart';
+import 'package:dw_barbershop/src/model/user_model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'login_vm.g.dart';
@@ -20,6 +21,13 @@ class LoginVm extends _$LoginVm {
 
     switch (result) {
       case Success():
+        final userModel = await ref.read(getMeProvider.future);
+        switch (userModel) {
+          case UserModelADM():
+            state = state.copyWith(status: LoginStateStatus.admLogin);
+          case UserModelEmployee():
+            state = state.copyWith(status: LoginStateStatus.employeeLogin);
+        }
         break;
       case Failure(exception: ServiceException(:final message)):
         state = state.copyWith(
