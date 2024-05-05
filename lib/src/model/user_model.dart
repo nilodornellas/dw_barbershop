4279779edcfbdc3a4b1956597cafc8eb2b446c55
Nilo Dornellas <iannilo01@gmail.com>
2024-05-05 09:25:36 -0300

    Implementando o modelo de usuário

---
+++ lib/src/model/user_model.dart
@@ -0,0 +1,85 @@
+sealed class UserModel {
+  final int id;
+  final String name;
+  final String email;
+  final String? avatar;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.avatar,
+  });
+}
+
+class UserModelADM extends UserModel {
+  final List<String>? workDays;
+  final List<int>? workHours;
+
+  UserModelADM({
+    required super.id,
+    required super.name,
+    required super.email,
+    super.avatar,
+    this.workDays,
+    this.workHours,
+  });
+
+  factory UserModelADM.fromMap(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': int id,
+        'name': String name,
+        'email': String email,
+      } =>
+        UserModelADM(
+          id: id,
+          name: name,
+          email: email,
+          avatar: json['avatar'],
+          workDays: json['work_days']?.cast<String>(),
+          workHours: json['work_hours']?.cast<int>(),
+        ),
+      _ => throw ArgumentError('Inavalid Json'),
+    };
+  }
+}
+
+class UserModelEmployee extends UserModel {
+  final int barbarshopId;
+  final List<String> workDays;
+  final List<int> workHours;
+
+  UserModelEmployee({
+    required super.id,
+    required super.name,
+    required super.email,
+    super.avatar,
+    required this.barbarshopId,
+    required this.workDays,
+    required this.workHours,
+  });
+
+  factory UserModelEmployee.fromMap(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': int id,
+        'name': String name,
+        'email': String email,
+        'barbarshop_id': int barbarshopId,
+        'work_days': List workDays,
+        'work_hours': List workHours,
+      } =>
+        UserModelEmployee(
+          id: id,
+          name: name,
+          email: email,
+          avatar: json['avatar'],
+          barbarshopId: barbarshopId,
+          workDays: workDays.cast<String>(),
+          workHours: workHours.cast<int>(),
+        ),
+      _ => throw ArgumentError('Inavalid Json'),
+    };
+  }
+}
